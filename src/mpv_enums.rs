d4a2959c4a6f992c0922b9fb8b00df6e51c5390a
Cobrand <cobrandw@gmail.com>
2016-05-19 16:51:35 +0200

    to_event update

--- src/mpv_enums.rs
+++ src/mpv_enums.rs
@@ -22,11 +22,12 @@ impl fmt::Display for MpvEventId {
 pub enum Event<'a,'b> {
     Shutdown,
     LogMessage,//(&'a str),
-    GetPropertyReply(&'a str,Result<Format<'b>>),
-    SetPropertyReply(Result<()>),
-    CommandReply,
+    GetPropertyReply(&'a str,Result<(Format<'b>,u32)>),
+    SetPropertyReply(Result<u32>),
+    CommandReply(Result<u32>),
     StartFile,
-    EndFile,
+    EndFile(Result<MpvEndFileReason>),
+    FileLoaded,
     TrackesSwitched,
     Idle,
     Pause,
@@ -38,17 +39,45 @@ pub enum Event<'a,'b> {
     MetadataUpdate,
     Seek,
     PlaybackRestart,
-    PropertyChange(&'a str,Format<'b>),
+    PropertyChange(&'a str,Format<'b>,u32),
     ChapterChange,
-    QueueOverflow
+    QueueOverflow,
+    /// Unused event
+    Unused
 }
 
 pub fn to_event<'a,'b>(event_id:MpvEventId,
                 error: c_int,
                 reply_userdata: c_ulong,
                 data:*mut c_void) -> Option<Event<'a,'b>> {
-                    None
-                }
+    match event_id {
+        MpvEventId::MPV_EVENT_NONE                  => None,
+        MpvEventId::MPV_EVENT_SHUTDOWN              => Some(Event::Shutdown),
+        MpvEventId::MPV_EVENT_LOG_MESSAGE           => unimplemented!(),
+        MpvEventId::MPV_EVENT_GET_PROPERTY_REPLY    => unimplemented!(),
+        MpvEventId::MPV_EVENT_SET_PROPERTY_REPLY    => unimplemented!(),
+        MpvEventId::MPV_EVENT_COMMAND_REPLY         => unimplemented!(),
+        MpvEventId::MPV_EVENT_START_FILE            => Some(Event::StartFile),
+        MpvEventId::MPV_EVENT_END_FILE              => unimplemented!(),
+        MpvEventId::MPV_EVENT_FILE_LOADED           => Some(Event::FileLoaded),
+        MpvEventId::MPV_EVENT_TRACKS_CHANGED        => Some(Event::Unused),
+        MpvEventId::MPV_EVENT_TRACK_SWITCHED        => Some(Event::TrackesSwitched),
+        MpvEventId::MPV_EVENT_IDLE                  => Some(Event::Idle),
+        MpvEventId::MPV_EVENT_PAUSE                 => Some(Event::Pause),
+        MpvEventId::MPV_EVENT_UNPAUSE               => Some(Event::Unpause),
+        MpvEventId::MPV_EVENT_TICK                  => Some(Event::Tick),
+        MpvEventId::MPV_EVENT_SCRIPT_INPUT_DISPATCH => Some(Event::Unused),
+        MpvEventId::MPV_EVENT_CLIENT_MESSAGE        => unimplemented!(),
+        MpvEventId::MPV_EVENT_VIDEO_RECONFIG        => Some(Event::VideoReconfig),
+        MpvEventId::MPV_EVENT_AUDIO_RECONFIG        => Some(Event::AudioReconfig),
+        MpvEventId::MPV_EVENT_METADATA_UPDATE       => Some(Event::MetadataUpdate),
+        MpvEventId::MPV_EVENT_SEEK                  => Some(Event::Seek),
+        MpvEventId::MPV_EVENT_PLAYBACK_RESTART      => Some(Event::PlaybackRestart),
+        MpvEventId::MPV_EVENT_PROPERTY_CHANGE       => unimplemented!(),
+        MpvEventId::MPV_EVENT_CHAPTER_CHANGE        => Some(Event::ChapterChange),
+        MpvEventId::MPV_EVENT_QUEUE_OVERFLOW        => Some(Event::QueueOverflow),
+    }
+}
 
 pub enum Format<'a>{
     Flag(bool),
