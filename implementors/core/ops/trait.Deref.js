dc392aca8a90edd180e2c4f072f09181766bce66
Cobrand <cobrandw@gmail.com>
2016-07-20 14:17:23 +0200

    Doc v0.2.1

---
+++ implementors/core/ops/trait.Deref.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["enum_primitive"] = [];implementors["mpv"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Deref.html' title='core::ops::Deref'>Deref</a> for <a class='struct' href='mpv/struct.MpvHandlerWithGl.html' title='mpv::MpvHandlerWithGl'>MpvHandlerWithGl</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Drop.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["mpv"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='mpv/struct.MpvHandlerWithGl.html' title='mpv::MpvHandlerWithGl'>MpvHandlerWithGl</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='mpv/struct.MpvHandler.html' title='mpv::MpvHandler'>MpvHandler</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
