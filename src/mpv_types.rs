6e92402a317c088d2aa743f91853302861f99321
Jonas Sprenger <jnspr@outlook.de>
2023-09-14 20:54:57 +0200

    Add function pointer type for GetProcAddress

--- src/mpv_types.rs
+++ src/mpv_types.rs
@@ -1,4 +1,7 @@
+use std::os::raw::{c_void, c_char};
 
 pub struct OsdString<'a> {
     pub string:&'a str
 }
+
+pub type GetProcAddressFn = unsafe extern "C" fn(ctx: *mut c_void, name: *const c_char) -> *mut c_void;
